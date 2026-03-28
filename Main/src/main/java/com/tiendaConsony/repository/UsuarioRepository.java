2fb65c810d352d5bb81f524f88d35d3c4cd5b3e7
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-03-27 22:09:28 -0600

    usuario repository creado

---
+++ Main/src/main/java/com/tiendaConsony/repository/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package com.tiendaConsony.repository;
+
+import com.tiendaConsony.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+}
