04a08fe7541debe200f40e3624eebc0a5fa8f163
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-03-27 18:27:29 -0600

    Proucto service creado y terminado

--- Main/src/main/java/com/tiendaConsony/service/ProductoService.java
+++ Main/src/main/java/com/tiendaConsony/service/ProductoService.java
@@ -1,4 +1,13 @@
 package com.tiendaConsony.service;
 
+import com.tiendaConsony.entity.Producto;
+
+import java.util.List;
+
 public interface ProductoService {
+    List<Producto> getAllProducto();
+    Producto getProductoById(Integer id);
+    Producto saveProducto(Producto producto) throws RuntimeException;
+    Producto updateProducto(Integer id, Producto producto);
+    void deleteProducto(Integer id);
 }
