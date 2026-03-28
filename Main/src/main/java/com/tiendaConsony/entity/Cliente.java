287634fde2a3e2b49ab8fb0fd79fd6905f54093f
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-03-27 19:14:54 -0600

    Entity de clientes terminada

---
+++ Main/src/main/java/com/tiendaConsony/entity/Cliente.java
@@ -0,0 +1,76 @@
+package com.tiendaConsony.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Clientes")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_cliente")
+    private Integer idProducto;
+
+    @Column(name = "nombre_cliente")
+    private String nombreCliente;
+
+    @Column(name = "apellido_cliente")
+    private String apellidoCliente;
+
+    @Column(name = "telefono_cliente")
+    private Integer telefonoCliente;
+
+    @Column(name = "correo_cliente")
+    private String correoCliente;
+
+    @Column(name = "direccion_cliente")
+    private String direccionCliente;
+
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    public void setNombreCliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+
+    public String getApellidoCliente() {
+        return apellidoCliente;
+    }
+
+    public void setApellidoCliente(String apellidoCliente) {
+        this.apellidoCliente = apellidoCliente;
+    }
+
+    public Integer getTelefonoCliente() {
+        return telefonoCliente;
+    }
+
+    public void setTelefonoCliente(Integer telefonoCliente) {
+        this.telefonoCliente = telefonoCliente;
+    }
+
+    public String getCorreoCliente() {
+        return correoCliente;
+    }
+
+    public void setCorreoCliente(String correoCliente) {
+        this.correoCliente = correoCliente;
+    }
+
+    public String getDireccionCliente() {
+        return direccionCliente;
+    }
+
+    public void setDireccionCliente(String direccionCliente) {
+        this.direccionCliente = direccionCliente;
+    }
+}
