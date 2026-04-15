a13077e41e24ef47d8e5f6baf18f83f1c18a406b
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-04-15 10:56:51 -0600

    se implementaron las acciones del service

--- Main/src/main/java/com/tiendaConsony/service/UsuarioServiceImplements.java
+++ Main/src/main/java/com/tiendaConsony/service/UsuarioServiceImplements.java
@@ -1,53 +1,52 @@
 package com.tiendaConsony.service;
 
 import com.tiendaConsony.entity.Usuario;
-import com.tiendaConsony.exception.ResourceNotFoundException;
 import com.tiendaConsony.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class UsuarioServiceImplements implements UsuarioService {
 
-    private final UsuarioRepository usuarioRepository;
+    private final UsuarioRepository repository;
 
-    public UsuarioServiceImplements(UsuarioRepository usuarioRepository) {
-        this.usuarioRepository = usuarioRepository;
+    @Autowired
+    public UsuarioServiceImplements(UsuarioRepository repository) {
+        this.repository = repository;
     }
 
     @Override
-    public List<Usuario> getAllUsuario() {
-        return usuarioRepository.findAll();
+    public Usuario registrar(String username, String contrasena){
+        if (repository.findByUsername(username) != null){
+            return null;
+        }
+
+        Usuario u = new Usuario();
+        u.setUsername(username);
+        u.setContrasena(contrasena);
+
+        return repository.save(u);
     }
 
     @Override
-    public Usuario getUsuarioById(Integer id) {
-        return usuarioRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Usuario no encontrado"));
+    public Usuario login(String username, String contrasena) {
+        Usuario u = repository.findByUsername(username);
+
+        if (u != null && u.getContrasena().equals(contrasena)){
+            return u;
+        }
+        return null;
     }
 
     @Override
-    public Usuario saveUsuario(Usuario usuario) {
-        return usuarioRepository.save(usuario);
+    public List<Usuario> listar() {
+        return repository.findAll();
     }
 
     @Override
-    public Usuario updateUsuario(Integer id, Usuario usuario) {
-        Usuario usuario1 = usuarioRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Usuario no encontrado"));
-        usuario1.setUsername(usuario.getUsername());
-        usuario1.setContrasena(usuario.getContrasena());
-        usuario1.setRol(usuario.getRol());
-
-        return usuarioRepository.save(usuario1);
-    }
-
-    @Override
-    public void deleteUsuario(Integer id) {
-        Usuario usuario = usuarioRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Usuario no encontrado"));
-
-        usuarioRepository.delete(usuario);
+    public void eliminar(int id) {
+        repository.deleteById(id);
     }
 }
