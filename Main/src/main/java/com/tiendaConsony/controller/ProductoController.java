a892116a7acbe9a61cb14076c4ba034a80946841
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-04-15 11:51:31 -0600

    se borro una importacion

--- Main/src/main/java/com/tiendaConsony/controller/ProductoController.java
+++ Main/src/main/java/com/tiendaConsony/controller/ProductoController.java
@@ -4,11 +4,12 @@ import com.tiendaConsony.entity.Producto;
 import com.tiendaConsony.service.ProductoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/api/producto")
 public class ProductoController {
 
