2c83db50f11c25ade0d1373fd9473a338fe14916
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-04-15 10:55:37 -0600

    se cambiaron las acciones del usuario service

--- Main/src/main/java/com/tiendaConsony/service/UsuarioService.java
+++ Main/src/main/java/com/tiendaConsony/service/UsuarioService.java
@@ -4,9 +4,8 @@ import com.tiendaConsony.entity.Usuario;
 import java.util.List;
 
 public interface UsuarioService {
-    List<Usuario> getAllUsuario();
-    Usuario getUsuarioById(Integer id);
-    Usuario saveUsuario(Usuario usuario) throws RuntimeException;
-    Usuario updateUsuario(Integer id, Usuario usuario);
-    void deleteUsuario(Integer id);
+    Usuario registrar(String username, String contrasena);
+    Usuario login(String username, String contrasena);
+    List<Usuario> listar();
+    void eliminar(int id);
 }
