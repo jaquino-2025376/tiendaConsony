8f6bd1c4e85f9dc3bcfee4a8caa19886982f1c3e
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-04-15 10:43:01 -0600

    refactor: se cambiaron algunos atributos

--- Main/src/main/java/com/tiendaConsony/service/ClienteServiceImplements.java
+++ Main/src/main/java/com/tiendaConsony/service/ClienteServiceImplements.java
@@ -1,7 +1,6 @@
 package com.tiendaConsony.service;
 
 import com.tiendaConsony.entity.Cliente;
-import com.tiendaConsony.exception.ResourceNotFoundException;
 import com.tiendaConsony.repository.ClienteRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,54 +8,34 @@ import java.util.List;
 
 @Service
 public class ClienteServiceImplements implements ClienteService{
-
     private final ClienteRepository clienteRepository;
 
-    public ClienteServiceImplements(ClienteRepository clienteRepository){
+    public ClienteServiceImplements(ClienteRepository clienteRepository) {
         this.clienteRepository = clienteRepository;
     }
 
     @Override
-    public List<Cliente> getAllCliente(){
+    public List<Cliente> getAllCliente() {
         return clienteRepository.findAll();
     }
 
     @Override
     public Cliente getClienteById(Integer id) {
-        Cliente cliente = clienteRepository.findById(id).orElse(null);
-        if (cliente == null) {
-            throw new ResourceNotFoundException("Cliente no encontrado");
-        }
-        return cliente;
+        return clienteRepository.findById(id).orElse(null);
     }
 
     @Override
-    public Cliente saveCliente(Cliente cliente){
+    public Cliente saveCliente(Cliente cliente) throws RuntimeException {
         return clienteRepository.save(cliente);
     }
 
     @Override
-    public Cliente updateCliente(Integer id, Cliente cliente){
-        Cliente updateCliente = clienteRepository.findById(id).orElse(null);
-        if (cliente == null){
-            updateCliente.setNombreCliente(cliente.getNombreCliente());
-            updateCliente.setApellidoCliente(cliente.getApellidoCliente());
-            updateCliente.setCorreoCliente(cliente.getCorreoCliente());
-            updateCliente.setTelefonoCliente(cliente.getTelefonoCliente());
-            updateCliente.setDireccionCliente(cliente.getDireccionCliente());
-        }else{
-            throw new ResourceNotFoundException("Cliente no encontrado");
-        }
-        return clienteRepository.save(updateCliente);
+    public Cliente updateCliente(Integer id, Cliente cliente) {
+        return clienteRepository.save(cliente);
     }
 
     @Override
     public void deleteCliente(Integer id) {
-        Cliente cliente = clienteRepository.findById(id).orElse(null);
-        if (cliente == null){
-            throw new ResourceNotFoundException("Cliente no encontrado");
-        }
-        clienteRepository.delete(cliente);
+        clienteRepository.deleteById(id);
     }
-
 }
