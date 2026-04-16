1849c21187e31be143cea66cad699a69fc70f281
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-04-15 21:42:38 -0600

    se arreglo un error de nomenclatura

--- Main/src/main/java/com/tiendaConsony/MainApplication.java
+++ Main/src/main/java/com/tiendaConsony/MainApplication.java
@@ -7,11 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MainApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MainApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
 
-    public void run(String... args) throws Exception{
+    @Override
+    public void run(String... args) throws Exception {
         System.out.println("Api funcionando");
     }
 }
