48f222617c97df67585ea1bd453600af5939bb8f
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-04-15 23:39:13 -0600

    refactor: se modifico y mejoro el controlador de la vista de ventas

--- Main/src/main/java/com/tiendaConsony/controller/VentasController.java
+++ Main/src/main/java/com/tiendaConsony/controller/VentasController.java
@@ -1,12 +1,22 @@
 package com.tiendaConsony.controller;
 
+import com.tiendaConsony.service.VentaService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VentasController {
+
+    private final VentaService service;
+
+    public VentasController(VentaService service) {
+        this.service = service;
+    }
+
     @GetMapping("/venta")
-    public String mostrarVentas(){
+    public String mostrarVentas(Model model) {
+        model.addAttribute("ventas", service.getAllVenta());
         return "venta";
     }
 }
