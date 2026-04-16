19ad160a6cc27bcf511a657db15856ec25f331a6
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-04-15 21:40:06 -0600

    refactor: Se añadio un metodo para poder listar los registros de la base de datos

--- Main/src/main/java/com/tiendaConsony/controller/ClientesController.java
+++ Main/src/main/java/com/tiendaConsony/controller/ClientesController.java
@@ -1,14 +1,22 @@
 package com.tiendaConsony.controller;
 
+import com.tiendaConsony.service.ClienteService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/cliente")
 public class ClientesController {
-    @GetMapping
-    public String mostrarCliente(){
-        return "cliente";
+
+        private final ClienteService service;
+
+        public ClientesController(ClienteService service) {
+            this.service = service;
+        }
+
+        @GetMapping("/cliente")
+        public String listarClientes(Model model){
+            model.addAttribute("clientes", service.getAllCliente());
+            return "cliente";
+        }
     }
-}
