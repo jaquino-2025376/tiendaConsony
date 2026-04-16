dddf0adb13a5920685a9c247c0d4d16b0f8d937b
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-04-15 23:37:51 -0600

    refactor: se modifico y mejoro el controlador de la vista de producto

--- Main/src/main/java/com/tiendaConsony/controller/ProductosController.java
+++ Main/src/main/java/com/tiendaConsony/controller/ProductosController.java
@@ -1,12 +1,22 @@
 package com.tiendaConsony.controller;
 
+import com.tiendaConsony.service.ProductoService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProductosController {
+
+    private final ProductoService service;
+
+    public ProductosController(ProductoService service) {
+        this.service = service;
+    }
+
     @GetMapping("/producto")
-    public String mostrarProducto(){
+    public String listarProductos(Model model){
+        model.addAttribute("productos", service.getAllProducto());
         return "producto";
     }
 }
