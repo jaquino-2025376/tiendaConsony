907b80046cdf880ca9b6267261cda10d13ef62d4
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-04-15 21:38:41 -0600

    controller logout creado para poder cerrar sesion del proyecto

---
+++ Main/src/main/java/com/tiendaConsony/controller/logoutController.java
@@ -0,0 +1,14 @@
+package com.tiendaConsony.controller;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class logoutController {
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/login";
+    }
+}
