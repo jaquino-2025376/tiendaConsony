05600815c3c77e1e46983dca6d723335eedc3d4c
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-04-15 21:40:22 -0600

    refactor: Se añadio @Service

--- Main/src/main/java/com/tiendaConsony/service/ClienteService.java
+++ Main/src/main/java/com/tiendaConsony/service/ClienteService.java
@@ -1,9 +1,11 @@
 package com.tiendaConsony.service;
 
 import com.tiendaConsony.entity.Cliente;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface ClienteService {
     List<Cliente> getAllCliente();
     Cliente getClienteById(Integer id);
