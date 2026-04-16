2a8cc30f5bb6a5ce74eab2bd55a848eccb288d42
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-04-15 23:35:41 -0600

    refactor: se modifico y mejoro el controlador de la vista de detalleVentas

--- Main/src/main/java/com/tiendaConsony/controller/DetallesVentaController.java
+++ Main/src/main/java/com/tiendaConsony/controller/DetallesVentaController.java
@@ -1,12 +1,23 @@
 package com.tiendaConsony.controller;
 
+import com.tiendaConsony.service.DetalleVentaService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DetallesVentaController {
-    @GetMapping("/detalleVentas")
-    public String mostrarDetalleVentas(){
-        return "detalleVentas";
+
+    private final DetalleVentaService service;
+
+    public DetallesVentaController(DetalleVentaService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/detalleVenta")
+    public String mostrarDetalleVentas(Model model){
+        model.addAttribute("detalles", service.getAllDetalleVenta());
+        return "detalleVenta";
     }
 }
