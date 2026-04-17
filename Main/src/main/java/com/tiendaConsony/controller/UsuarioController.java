b895a9d864212429e0ac08c3d5a61d56e8c3a186
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-04-17 16:40:37 -0600

    refactor: se añadieron los atributos del rol y de la sesion

--- Main/src/main/java/com/tiendaConsony/controller/UsuarioController.java
+++ Main/src/main/java/com/tiendaConsony/controller/UsuarioController.java
@@ -2,6 +2,7 @@ package com.tiendaConsony.controller;
 
 import com.tiendaConsony.entity.Usuario;
 import com.tiendaConsony.service.UsuarioService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,11 +24,15 @@ public class UsuarioController {
     @PostMapping("/login")
     public String validar(@RequestParam String username,
                           @RequestParam String contrasena,
+                          HttpSession session,
                           Model model) {
 
         Usuario u = service.login(username, contrasena);
 
         if (u != null) {
+            session.setAttribute("usuarioSesion", u);
+            session.setAttribute("rolSesion",     u.getRol() != null ? u.getRol() : "SIN ROL");
+            session.setAttribute("fotoSesion",    u.getUsername() + ".jpg");
             return "redirect:/inicio";
         } else {
             model.addAttribute("error", "Credenciales Incorrectas");
@@ -43,22 +48,23 @@ public class UsuarioController {
     @PostMapping("/registro")
     public String guardar(@RequestParam String username,
                           @RequestParam String contrasena,
+                          @RequestParam(required = false, defaultValue = "VENDEDOR") String rol,
                           Model model) {
 
-        Usuario u = service.registrar(username, contrasena);
-
+        Usuario u = service.registrar(username, contrasena, rol);
         if (u == null) {
             model.addAttribute("error", "Usuario ya existente");
             return "registro";
         }
-
         return "redirect:/login";
     }
 
     @GetMapping("/lista")
-    public String listar(Model model) {
-        List<Usuario> lista = service.listar();
-        model.addAttribute("usuarios", lista);
+    public String listar(Model model, HttpSession session) {
+        model.addAttribute("usuarios",      service.listar());
+        model.addAttribute("usuarioSesion", session.getAttribute("usuarioSesion"));
+        model.addAttribute("rolSesion",     session.getAttribute("rolSesion"));
+        model.addAttribute("fotoSesion",    session.getAttribute("fotoSesion"));
         return "lista";
     }
 
