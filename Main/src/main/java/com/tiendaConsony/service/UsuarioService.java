0c140af037b95b9d1d54f534a631606733d3ec3d
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-04-17 16:41:02 -0600

    refactor: se añadio una nueva variante para el registrar

--- Main/src/main/java/com/tiendaConsony/service/UsuarioService.java
+++ Main/src/main/java/com/tiendaConsony/service/UsuarioService.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public interface UsuarioService {
     Usuario registrar(String username, String contrasena);
+    Usuario registrar(String username, String contrasena, String rol);
     Usuario login(String username, String contrasena);
     List<Usuario> listar();
     void eliminar(int id);
